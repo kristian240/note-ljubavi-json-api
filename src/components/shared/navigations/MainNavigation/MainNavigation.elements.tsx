96e4b6802f50ce8be336b3cc8ba386661167846c
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 01:09:46 +0200

    Update main navigation

--- src/components/shared/navigations/MainNavigation/MainNavigation.elements.tsx
+++ src/components/shared/navigations/MainNavigation/MainNavigation.elements.tsx
@@ -4,6 +4,6 @@ export const NavigationWrapper = chakra('nav', {
 	baseStyle: {
 		py: 6,
 		mx: 'auto',
-		maxWidth: '800px',
+		maxW: 'lg',
 	},
 });
--- src/components/shared/navigations/MainNavigation/MainNavigation.tsx
+++ src/components/shared/navigations/MainNavigation/MainNavigation.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
-import { Box, Flex, Link, useColorMode, IconButton, Button, Heading, LinkBox, LinkOverlay } from '@chakra-ui/react';
+import { Box, Flex, Heading, HStack, IconButton, Link, LinkBox, LinkOverlay, useColorMode } from '@chakra-ui/react';
+import { ariaAttr } from '@chakra-ui/utils';
 import NextLink from 'next/link';
+import { useRouter } from 'next/dist/client/router';
 
 import { NavigationWrapper } from './MainNavigation.elements';
 
@@ -8,30 +10,35 @@ import MoonIcon from '@/assets/icons/ic-moon.svg';
 import SunIcon from '@/assets/icons/ic-sun.svg';
 
 export const MainNavigation: FC = () => {
+	const { pathname } = useRouter();
 	const { colorMode, toggleColorMode } = useColorMode();
 
 	return (
 		<NavigationWrapper>
-			<Flex justifyContent="space-between" alignItems="center">
+			<Flex justify="space-between" align="center">
 				<LinkBox>
 					<NextLink href="/" passHref>
-						<Link>
+						<LinkOverlay>
 							<Heading as="span" size="lg">
-								Todo
+								Note Ljubavi
 							</Heading>
-						</Link>
+						</LinkOverlay>
 					</NextLink>
 				</LinkBox>
-				<Box>
-					<Button disabled mr={2}>
-						Logout
-					</Button>
+
+				<HStack spacing={4}>
+					<NextLink href="/songs">
+						<Heading as={Link} size="sm" aria-current={ariaAttr(['/songs', '/songs/[id]'].includes(pathname))}>
+							Pjesme
+						</Heading>
+					</NextLink>
+
 					<IconButton
 						aria-label="Toggle color mode"
 						onClick={toggleColorMode}
 						icon={<Box w="16px" as={colorMode === 'light' ? MoonIcon : SunIcon} />}
 					/>
-				</Box>
+				</HStack>
 			</Flex>
 		</NavigationWrapper>
 	);
