f968607a7f2a5e51b51f15909d69db05a2900be1
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 01:12:39 +0200

    Add missing resource to AppStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { Client } from '@datx/jsonapi-react';
 
 import { Author } from '@/resources/Author';
+import { Song } from '@/resources/Song';
 
 class AppCollection extends Client {
-	public static types = [Author];
+	public static types = [Author, Song];
 }
 
 export default new AppCollection();
