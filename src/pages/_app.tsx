6889c61cb1bbc535dfa7820deb119d8f2a902575
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 00:49:57 +0200

    Remove custom fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { DatxProvider } from '@datx/jsonapi-react';
 import { DefaultSeo } from 'next-seo';
 
 import theme from '../styles/theme';
-import { Fonts } from '../styles/Fonts';
 import client from '../store';
 
 import '../store/utils/config';
@@ -16,7 +15,6 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
 	return (
 		<DatxProvider client={client}>
 			<ChakraProvider theme={theme}>
-				<Fonts />
 				<DefaultSeo {...defaultSeoProps}>
 					<Component {...pageProps} />
 				</DefaultSeo>
