d7d91a1a33f45cc2e027ad0348a1722c40dd800c
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 00:48:24 +0200

    Add next SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,8 @@
+import { DefaultSeoProps } from 'next-seo';
+
+const defaultSeoProps: DefaultSeoProps = {
+	titleTemplate: '%s | Note Ljubavi',
+	defaultTitle: 'Note Ljubavi',
+};
+
+export default defaultSeoProps;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { DatxProvider } from '@datx/jsonapi-react';
+import { DefaultSeo } from 'next-seo';
 
 import theme from '../styles/theme';
 import { Fonts } from '../styles/Fonts';
@@ -9,12 +10,16 @@ import client from '../store';
 
 import '../store/utils/config';
 
+import defaultSeoProps from 'next-seo.config';
+
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
 	return (
 		<DatxProvider client={client}>
 			<ChakraProvider theme={theme}>
 				<Fonts />
-				<Component {...pageProps} />
+				<DefaultSeo {...defaultSeoProps}>
+					<Component {...pageProps} />
+				</DefaultSeo>
 			</ChakraProvider>
 		</DatxProvider>
 	);
