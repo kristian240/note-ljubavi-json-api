474e9fb5865b6779ea5db1c1027df3ff666b27e9
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 20:19:04 +0200

    Update LoadingMessage component

--- src/components/shared/messages/LoadingMessage/LoadingMessage.tsx
+++ src/components/shared/messages/LoadingMessage/LoadingMessage.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react';
-import { Center, CenterProps, Image, Text } from '@chakra-ui/react';
+import { Center, CenterProps, HStack, Spinner, Text } from '@chakra-ui/react';
 
-export const LoadingMessage: FC<CenterProps> = (props) => (
-	<Center height="100%" {...props}>
-		<Image src="/images/logo.png" alt="presentation" width="240px" />
-		<Text ml={10} fontSize="3xl">
-			Loading...
-		</Text>
+interface ILoadingMessage extends CenterProps {
+	message?: string;
+}
+
+export const LoadingMessage: FC<ILoadingMessage> = ({ message, ...rest }) => (
+	<Center h="full" {...rest}>
+		<HStack spacing={4}>
+			<Spinner />
+
+			<Text ml={10} fontSize="3xl">
+				{message}
+			</Text>
+		</HStack>
 	</Center>
 );
+
+LoadingMessage.defaultProps = {
+	message: 'Učitavanje...',
+};
