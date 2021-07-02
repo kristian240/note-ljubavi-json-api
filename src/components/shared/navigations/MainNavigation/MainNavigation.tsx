acded5152c944a7e0ec04021e59425a07f9134d7
kristian240 <kristian.djakovic@infinum.hr>
2021-07-02 21:03:02 +0200

    Update navigation

--- src/components/shared/navigations/MainNavigation/MainNavigation.tsx
+++ src/components/shared/navigations/MainNavigation/MainNavigation.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Flex, Heading, HStack, Icon, IconButton, Link, LinkBox, LinkOverlay, useColorMode } from '@chakra-ui/react';
-import { ariaAttr } from '@chakra-ui/utils';
+import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
 import { useRouter } from 'next/dist/client/router';
 
 import { NavigationWrapper } from './MainNavigation.elements';
 
+import LogoIcon from '@/assets/icons/ic-logo.svg';
 import MoonIcon from '@/assets/icons/ic-moon.svg';
 import SunIcon from '@/assets/icons/ic-sun.svg';
 
@@ -16,6 +17,15 @@ export const MainNavigation: FC = () => {
 	return (
 		<NavigationWrapper>
 			<Flex justify="space-between" align="center" color={colorMode === 'light' ? 'primary.700' : 'white'}>
+				<NextLink href="/" passHref>
+					<IconButton
+						as={Link}
+						aria-label="Početna stranica"
+						variant="ghost"
+						icon={<Icon as={LogoIcon} boxSize={6} />}
+					/>
+				</NextLink>
+
 				<LinkBox>
 					<NextLink href="/" passHref>
 						<LinkOverlay>
@@ -26,17 +36,22 @@ export const MainNavigation: FC = () => {
 					</NextLink>
 				</LinkBox>
 
-				<HStack spacing={4}>
+				<HStack spacing={2}>
 					<NextLink href="/songs" passHref>
-						<Heading as={Link} size="sm" aria-current={ariaAttr(['/songs', '/songs/[id]'].includes(pathname))}>
+						<Heading
+							as={Link}
+							size="sm"
+							aria-current={['/songs', '/songs/[id]'].includes(pathname) ? 'page' : undefined}
+							display={{ base: 'none', lg: 'block' }}
+						>
 							Pjesme
 						</Heading>
 					</NextLink>
 
 					<IconButton
-						aria-label="Toggle color mode"
+						aria-label="Promjeni temu"
 						onClick={toggleColorMode}
-						icon={<Icon boxSize="16px" as={colorMode === 'light' ? MoonIcon : SunIcon} />}
+						icon={<Icon boxSize={4} as={colorMode === 'light' ? MoonIcon : SunIcon} />}
 					/>
 				</HStack>
 			</Flex>
